606e6e2b2d87d9a47d9b7eeb39379cc49b080971
Camila Cangussu <camila.katheryne@gmail.com>
2022-07-30 10:45:11 -0300

    Removes unnecessary files and change sk

--- rental_system/settings.py
+++ rental_system/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pau&(ehrw8_x@+r#dmp_z8a(_3m(+(o0$teo*m6vy=wg&3_zhj'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
